067b04a3b49a9d052c36b66f5002e7e6dacf64e8
oliver <oliverbodden@hotmail.com>
2022-06-02 18:12:29 -0400

    Fixing bugs

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ mongoose.connect(
   `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
   {
     useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
   }
 );
 
