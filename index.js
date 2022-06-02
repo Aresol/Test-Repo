9f14db22f8248f77a2d5215c25506bbfb9e9949d
oliver <oliverbodden@hotmail.com>
2022-06-02 17:24:30 -0400

    Fixing bugs

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ import dotenv from "dotenv"
 dotenv.config()
 const port = process.env.port || 8000 // If cannot access set to 8000
 
-//CATCH ERROR
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-}) 
-
 //Start the connection with the parameters
 .then(async client => {
     app.listen(port, () => {
