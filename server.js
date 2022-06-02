74cd1683c796ee5a1883997a04146f55c18a5764
oliver <oliverbodden@hotmail.com>
2022-06-02 17:49:55 -0400

    Fixing bugs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express"
-import cors from "cors"
-import wpp1 from "./api/wpp1.route.js"
+const express = require("express");
+const mongoose = require("mongoose");
+const Router = require("./api/route")
 
 const app = express()
 
@@ -9,5 +9,25 @@ app.use(express.json()) // Allows express to read json
 
 app.use("/api/v1/wpp1", wpp1) // URL for users to go to the app
 app.use("*", (req, res) => res.status(404).json({ error: "not found"})) // If used an incorrect URL return error
+
+mongoose.connect(process.env.WPP1_DB_URI,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(Router);
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
 
 export default app
