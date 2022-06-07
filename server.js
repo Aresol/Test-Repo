ce3b6f693b9b85df83adab8bfc0176530e26e1a3
oliver <oliverbodden@hotmail.com>
2022-06-07 19:18:06 -0400

    new server

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-import express from "express"
-import mongoose from "mongoose"
-import router from "./route.js"
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const username = "wpp-mdb-admin";
-const password = "UbMZwDLZCKGSum4t";
-const cluster = "cluster0.blzddpr";
-const dbname = "";
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json()) // Allows express to read json
 
-mongoose.connect(
-  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-  {
-    useNewUrlParser: true
-  }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+app.listen(port, () => {
+  console.log('Server is running at port: ${port}');
 });
 
 export default app
